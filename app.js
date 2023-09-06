7fa2a9e3c7663805f9133467d84cf573201e6e53
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-09-06 14:55:59 +0900

    Feat : SpecialAttack

--- app.js
+++ app.js
@@ -3,18 +3,38 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
+  },
+
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+    useSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
 
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
       this.monsterHealth -= attackValue;
       this.attackPlayer();
+      console.log(this.currentRound);
     },
     attackPlayer() {
       const attackValue = Math.floor(Math.random() * (15 - 8)) + 8;
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      const attackValue = Math.floor(Math.random() * (25 - 10)) + 10;
+      this.monsterHealth -= attackValue;
+      this.attackMonster();
     },
   },
 });
