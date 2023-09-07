d54bfa2664c38d69100e07e692e7d1b7dc9160d2
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-09-07 15:03:49 +0900

    Feat : Heal Function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
 
@@ -16,6 +17,23 @@ const app = Vue.createApp({
     },
     useSpecialAttack() {
       return this.currentRound % 3 !== 0;
+    },
+  },
+
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
     },
   },
 
@@ -35,6 +53,16 @@ const app = Vue.createApp({
       const attackValue = Math.floor(Math.random() * (25 - 10)) + 10;
       this.monsterHealth -= attackValue;
       this.attackMonster();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = Math.floor(Math.random() * (20 - 8)) + 8;
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
     },
   },
 });
